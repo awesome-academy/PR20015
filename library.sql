d33a4d5a9224676acf199ffd249d13da297e73e9
nguyenThiTham98 <nttham98@gmail.com>
2020-07-17 08:38:07 +0700

    nguyenThiTham

--- library.sql
+++ library.sql
@@ -151,11 +151,11 @@ values
 
 
 -- a. List the books have been borrowed more than 10 times in 2011
-select title 
+/*select title 
 	from book inner join borrow_detail on book.book_id=borrow_detail.book_id inner join borrow on borrow_detail.borrow_id=borrow.borrow_id
 	where year(borrow_date)=2011
     group by title 
-    having SUM(quantity)>=10
+    having SUM(quantity)>=10*/
 
 -- b. List the readers has borrowed at least 10 different books
 /*select reader_name 
